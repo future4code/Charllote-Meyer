7930df0b8b42d2660dd540abaa8a149eee6fd0d5
CharlloteM <charllotem@gmail.com>
2021-09-10 23:26:43 -0300

    exercícios_operadores_aula04_javascript

---
+++ quinzena1/operadores/operadores.js
@@ -0,0 +1,98 @@
+// ----- EXERCÍCIOS DE INTERPRETAÇÃO DE CÓDIGO ------------------------
+
+// 1. ----------------------------------------------------------
+
+ // Resposta: false, false, true e boolean
+ /*
+        const bool1 = true
+        const bool2 = false
+        const bool3 = !bool2
+
+        let resultado = bool1 && bool2
+        console.log("a. ", resultado) // true E false -> false
+
+        resultado = bool1 && bool2 && bool3 
+        console.log("b. ", resultado) // true E false E false -> false
+
+        resultado = !resultado && (bool1 || bool2) 
+        console.log("c. ", resultado) // true OU false -> true
+
+        console.log("d. ", typeof resultado) // boolean
+*/
+
+// 2. ----------------------------------------------------------
+
+        // Resposta: faltava o NUMBER pro código rodar corretamente.
+
+// 3. ----------------------------------------------------------
+
+// Resolução do exercício anterior.
+/*
+        let primeiroNumero = Number(prompt("Digite um número!"))
+        let segundoNumero = Number(prompt("Digite outro número!"))
+
+        const soma = primeiroNumero + segundoNumero
+
+        console.log(soma)
+*/
+
+// ----- EXERCÍCIOS DE EXCRITA DE CÓDIGO ------------------------
+
+// 1. ----------------------------------------------------------
+/*
+        const suaIdade = Number(prompt("Digite sua idade."))
+        const idadeAmigo = Number(prompt("Digite a idade do(a) seu(ua) melhor amigo(a)."))
+
+        const soma = suaIdade + idadeAmigo
+
+        console.log("Sua idade é maior que a do seu(ua) melhor amigo(a)?", suaIdade >= idadeAmigo) 
+
+        console.log(soma)
+*/
+
+// 2. ----------------------------------------------------------
+/*
+        const numeroPar = Number(prompt("Insira aqui um número PAR."))
+
+        const divisao = numeroPar - 2
+
+        console.log(divisao)
+// a divisão funciona para todos os números pares, sempre diminuíndo em dois números.
+// a divisão também ocorre para todos os números ímpares, sempre diminuíndo em dois números também.
+*/
+
+// 3. ----------------------------------------------------------
+/*
+        const suaIdade = Number(prompt("Digite sua idade."))
+
+        console.log("sua idade em meses é:", suaIdade * 12)
+        console.log("Sua idade em dias é:", suaIdade * 365)
+        console.log("Sua idade em horas é:", suaIdade * 8760)
+*/
+
+// 4. ----------------------------------------------------------
+/*
+        const numero1 = Number(prompt("Digite aqui um número qualquer."))
+        const numero2 = Number(prompt("Digite aqui outro número qualquer."))
+
+        console.log(numero1)
+        console.log(numero2)
+
+        console.log(numero1 > numero2)
+        console.log(numero1 === numero2)
+        console.log(numero1 % numero2)
+        console.log(numero2 % numero1)
+*/
+
+// ----- DESAFIOS ---------------------------------------------
+
+// 1. ---------------------------------------------------------
+
+
+
+// 2. ---------------------------------------------------------
+
+
+
+// 3. ---------------------------------------------------------
+
